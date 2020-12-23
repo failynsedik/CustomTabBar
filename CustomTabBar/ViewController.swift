3a53486cdbc4efdf9bad74af6384773549cd94c9
Failyn Kaye Sedik <failynsedik@gmail.com>
2020-12-23 13:59:18 +0800

    Initial Commit

---
+++ CustomTabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTabBar
+//
+//  Created by Failyn Kaye M. Sedik on 12/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
