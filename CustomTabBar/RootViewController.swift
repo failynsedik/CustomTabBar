20910595859285a994f6dc97c5f0f3027d7d9b78
Failyn Kaye Sedik <failynsedik@gmail.com>
2020-12-23 14:06:06 +0800

    Add initial project configuration

--- CustomTabBar/RootViewController.swift
+++ CustomTabBar/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  CustomTabBar
 //
 //  Created by Failyn Kaye M. Sedik on 12/23/20.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class RootViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
