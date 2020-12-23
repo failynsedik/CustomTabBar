0beb5caca3b3ebfbecb30c7258231af6235d1d46
Failyn Kaye Sedik <failynsedik@gmail.com>
2020-12-23 15:13:45 +0800

    Add empty view controllers for the tab bar

---
+++ CustomTabBar/Controller/FavoritesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesViewController.swift
+//  CustomTabBar
+//
+//  Created by Failyn Kaye M. Sedik on 12/23/20.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ CustomTabBar/Controller/RecentsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RecentsViewController.swift
+//  CustomTabBar
+//
+//  Created by Failyn Kaye M. Sedik on 12/23/20.
+//
+
+import UIKit
+
+class RecentsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
